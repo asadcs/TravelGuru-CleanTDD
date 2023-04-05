ec7b4769d696cc05888c1eb3d09516282da685f6
Muhammad.fig <Muhammad@fig-api.com>
2023-04-06 02:54:27 +0500

    ValueObjects and Custom Exceptions Added

---
+++ src/PackIT.Domain/Entities/PackingList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PackIT.Domain.Entities
+{
+    public class PackingList
+    {
+        public Guid id { get; private set; }
+        private PackingList _name;
+        private string _localization;
+
+    }
+}
